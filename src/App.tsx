d3574a6d990e6abe87c8d87907237f6cff614ddb
ntqnhanguyen <139726337+ntqnhanguyen@users.noreply.github.com>
2025-05-30 15:54:22 +0700

    Implement remember login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
       <AuthProvider>
         <ThemeProvider>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/login\" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route
               path="/*"
