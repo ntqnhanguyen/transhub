1437bc27f88c87a1f9671d9ad183a3561ea3eb44
ntqnhanguyen <139726337+ntqnhanguyen@users.noreply.github.com>
2025-05-30 22:27:12 +0700

    Update Teams Component

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -120,7 +120,6 @@ export default function Teams() {
         })
         .select()
         .single();
-      console.log(teamError)
       if (teamError) throw teamError;
 
       // Add creator as team owner
